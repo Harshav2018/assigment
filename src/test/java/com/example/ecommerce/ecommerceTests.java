ebeddc183a65e55bf1a07f55b29d27de3a897973
Harsha <harsha.v2018@gmail.com>
2025-07-15 17:52:14 +0530

    test

--- src/test/java/com/example/ecommerce/ecommerceTests.java
+++ src/test/java/com/example/ecommerce/ecommerceTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BadStoreApplicationTests {
+class ecommerceTests {
 
     @Test
     void contextLoads() {
